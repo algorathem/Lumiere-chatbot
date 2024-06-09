b776ebfd5e6782aee596e6146f5990a5a0e775ea
kcl <kcleck06@gmail.com>
2024-06-09 20:11:04 +0800

    testing

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
-# pip install openai
-
 from openai import AzureOpenAI
+from dotenv import load_dotenv
+import os
 
-ENDPOINT = "https://polite-ground-030dc3103.4.azurestaticapps.net/api/v1"
-API_KEY = "INSERT_API_KEY"
+
+load_dotenv()
+
+
+ENDPOINT = os.environ.get('ENDPOINT')
+API_KEY = os.environ.get('API_KEY')
 
 API_VERSION = "2024-02-01"
 MODEL_NAME = "gpt-35-turbo"
@@ -15,19 +19,17 @@ client = AzureOpenAI(
     api_version=API_VERSION,
 )
 
+
 MESSAGES = [
-    {"role": "system", "content": "You are an empathetic therapist."},
-    {"role": "user", "content": "hello"},
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
     {
         "role": "assistant",
-        "content": "Hello, how can I help you?",
+        "content": "The Los Angeles Dodgers won the World Series in 2020.",
     },
-    {"role": "user", "content": "i feel (.*)"},
-    {
-        "role": "assistant",
-        "content": "Why do you feel {}?"
-    }
+    {"role": "user", "content": "Where was it played?"},
 ]
+
 
 completion = client.chat.completions.create(
     model=MODEL_NAME,
@@ -39,22 +41,17 @@ print(completion.model_dump_json(indent=2))
 
 app = Flask(__name__)
 
-# def get_completion(prompt, model="gpt-3.5-turbo"):
-#     messages = [{"role": "user", "content": prompt}]
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=messages,
-#         temperature=0, # this is the degree of randomness of the model's output
-#     )
-#   return response.choices[0].message["content"]
+
 @app.route("/")
 def home():    
     return render_template("index.html")
 @app.route("/get")
 def get_bot_response():    
     userText = request.args.get('msg')  
+    print("Usertext: "+ userText)
     response = completion.choices[0].message.content
-    #return str(bot.get_response(userText)) 
+    print("Response: "+response)
     return response
+    
 if __name__ == "__main__":
   app.run()
