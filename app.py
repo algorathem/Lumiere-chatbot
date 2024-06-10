1b321bb4cb2df703f0a95db69d41d82b70967951
kcl <kcleck06@gmail.com>
2024-06-10 21:53:58 +0800

    fix bug

--- app.py
+++ app.py
@@ -6,53 +6,44 @@ import os
 
 load_dotenv()
 
+app = Flask(__name__)
 
 ENDPOINT = os.environ.get('ENDPOINT')
 API_KEY = os.environ.get('API_KEY')
+
+print (ENDPOINT)
+print (API_KEY)
 
 API_VERSION = "2024-02-01"
 MODEL_NAME = "gpt-35-turbo"
 
 client = AzureOpenAI(
-    azure_endpoint=ENDPOINT,
-    api_key=API_KEY,
-    api_version=API_VERSION,
+ azure_endpoint=ENDPOINT,
+ api_key=API_KEY,
+ api_version=API_VERSION,
 )
-
-
-MESSAGES = [
-    {"role": "system", "content": "You are a friendly companion and comforts people when they are sad, celebrate successes with them."},
-    {"role": "user", "content": "Hello"},
-    {
-        "role": "assistant",
-        "content": "Hey, how are you doing?",
-    },
-    {"role": "user", "content": "I am bored."},
-    {"role": "assistant", "content": "Why do you think you are bored?"}
-]
-
-
-completion = client.chat.completions.create(
-    model=MODEL_NAME,
-    messages=MESSAGES,
-    temperature=1.0
-)
-
-
-
-app = Flask(__name__)
-
 
 @app.route("/")
-def home():    
-    return render_template("index.html")
+def home(): 
+ return render_template("index.html")
 @app.route("/get")
-def get_bot_response():    
-    userText = request.args.get('msg')  
-    print("Usertext: "+ userText)
-    response = completion.choices[0].message.content
-    print("Response: "+response)
-    return response
-    
+def get_bot_response(): 
+ userText = request.args.get('msg') 
+ print("Usertext: "+ userText)
+
+ MESSAGES = [
+ {"role": "system", "content": "You are a friendly companion and comforts people when they are sad, celebrate successes with them. If you are asked for your name, introduce yourself as Lumiere. If you are asked to answer questions not related to mental well-being, explain that you don't know the answer. If you are tasked to perform tasks not directly related to the user's mental state, explain that it is beyond you."},
+ {"role": "user", "content": userText},
+]
+ completion = client.chat.completions.create(
+ model=MODEL_NAME,
+ messages=MESSAGES,
+ temperature=1.0
+ )
+ response = completion.choices[0].message.content
+ print("Response: "+response)
+ 
+ return response
+ 
 if __name__ == "__main__":
-  app.run()
+ app.run()
